6b80a8e728764408d02e124e4c4a0773a0877a91
RemansiSavaliya <157030569+RemansiSavaliya@users.noreply.github.com>
2024-07-05 08:52:13 +0530

    Add files via upload

---
+++ r.rb
@@ -0,0 +1,20 @@
+a=10
+b=20
+c=15
+puts "value of a is:#{a}, c is #{c},& b is: #{b}"
+
+puts " value of a+b=#{a+b}"
+#if a>b
+#	puts "a is greter"
+#else
+#	puts"b is greter"
+
+	
+#end
+ # if a>b && a>c
+ # 	puts" a is gt"
+ # 	 elsif b>c
+ #           puts"b is gt"
+ #           else
+ #           puts"c is gt"
+ #           end 	 
